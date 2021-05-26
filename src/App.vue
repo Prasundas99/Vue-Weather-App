0180500d63b5d75a4f3d6bbb46d36347bb480f91
Prasundas99 <prasundas115@gmail.com>
2021-05-26 16:50:25 +0530

    App finalised

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
- 
+<Style />
+<Weather />
 </template>
+  
 
 <script>
+//importing style
+import Style from './components/Styles' 
+//importing components
+import Weather from './components/Weather'
 export default {
-  name: 'App',
+  name: "app",
+    components: {
+    Weather,
+    Style
+  }
 }
+
 </script>
 
-<style>
-
-</style>
